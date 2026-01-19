63b4b9bddf356ec9b596ebe5badf6156b69080d4
vasani2232-alt <vasani2232@.in>
2026-01-19 23:05:48 +0530

    java script

---
+++ first.js
@@ -0,0 +1,14 @@
+/*fullname = "Tony stark";
+age = 19;
+price = 99.99;
+radius = 14;
+x = null; 
+y = undefined;
+isFollow = true;
+fullname = 28;
+console.log(fullname); */
+fullname = "vidhi vasani";
+FULLNAME = "vidhi";
+
+console.log(fullname);
+console.log(FULLNAME);
